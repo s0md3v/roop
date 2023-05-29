0f782ff516ad2db61816787c6291d3d104f75747
henryruhs <info@henryruhs.com>
2023-05-30 01:28:42 +0200

    Move process time to the correct place

--- run.py
+++ run.py
@@ -51,8 +51,8 @@ def start_processing():
     start_time = time.time()
     if args['gpu']:
         process_video(args['source_img'], args["frame_paths"])
-        print(flush=True)
         end_time = time.time()
+        print(flush=True)
         print(f"Processing time: {end_time - start_time:.2f} seconds", flush=True)
         return
     frame_paths = args["frame_paths"]
@@ -65,8 +65,8 @@ def start_processing():
         p.get()
     pool.close()
     pool.join()
-    print(flush=True)
     end_time = time.time()
+    print(flush=True)
     print(f"Processing time: {end_time - start_time:.2f} seconds", flush=True)
 
 
