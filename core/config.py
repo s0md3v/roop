b6efc0bed8d2a7050fe8fa98650fa50aeb0d8096
Somdev Sangwan <s0md3v@gmail.com>
2023-05-29 17:50:26 +0530

    handle no-face cases

--- core/config.py
+++ core/config.py
@@ -7,4 +7,7 @@ face_analyser.prepare(ctx_id=0, det_size=(640, 640))
 
 def get_face(img_data):
     analysed = face_analyser.get(img_data)
-    return sorted(analysed, key=lambda x: x.bbox[0])[0]
+    try:
+        return sorted(analysed, key=lambda x: x.bbox[0])[0]
+    except IndexError:
+        return None
