8701123ee3b2f1e77c3e1c6594f62c87dd694ada
henryruhs <info@henryruhs.com>
2023-05-30 00:40:02 +0200

    Add GPU support, Quit on missing model, Remove globals (sorry)

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
+<<<<<<< HEAD
 import sys
 import time
+=======
+
+>>>>>>> 54f800d (Add GPU support, Quit on missing model, Remove globals (sorry))
 import torch
 import shutil
-import core.globals
 
 if not shutil.which('ffmpeg'):
     print('ffmpeg is not installed. Read the docs: https://github.com/s0md3v/roop#installation.\n' * 10)
     quit()
-if '--gpu' not in sys.argv:
-    core.globals.providers = ['CPUExecutionProvider']
 
 import glob
 import argparse
