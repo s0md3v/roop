41c69d318dd041d58f61576e8d860b9967467244
henryruhs <info@henryruhs.com>
2023-05-30 16:29:51 +0200

    Create face analyser and face swapper instance on demand

--- core/processor.py
+++ core/processor.py
@@ -1,11 +1,16 @@
-import os
 import cv2
 import insightface
-import core.globals
 from core.config import get_face
 from core.utils import rreplace
 
-face_swapper = insightface.model_zoo.get_model('inswapper_128.onnx', providers=core.globals.providers)
+FACE_SWAPPER = None
+
+
+def get_face_swapper():
+    global FACE_SWAPPER
+    if FACE_SWAPPER is None:
+        FACE_SWAPPER = insightface.model_zoo.get_model('inswapper_128.onnx')
+    return FACE_SWAPPER
 
 
 def process_video(source_img, frame_paths):
@@ -15,12 +20,13 @@ def process_video(source_img, frame_paths):
         try:
             face = get_face(frame)
             if face:
-                result = face_swapper.get(frame, face, source_face, paste_back=True)
+                result = get_face_swapper().get(frame, face, source_face, paste_back=True)
                 cv2.imwrite(frame_path, result)
                 print('.', end='', flush=True)
             else:
                 print('S', end='', flush=True)
         except Exception as e:
+            print(e, flush=True)
             print('E', end='', flush=True)
             pass
 
@@ -30,6 +36,6 @@ def process_img(source_img, target_path):
     face = get_face(frame)
     source_face = get_face(cv2.imread(source_img))
     result = face_swapper.get(frame, face, source_face, paste_back=True)
-    target_path = rreplace(target_path, "/", "/swapped-", 1) if "/" in target_path else "swapped-"+target_path
+    target_path = rreplace(target_path, "/", "/swapped-", 1) if "/" in target_path else "swapped-" + target_path
     print(target_path)
     cv2.imwrite(target_path, result)
