1cc34ce8e0263b6d7e6e64268f12b24ebf5aa1ab
Somdev Sangwan <s0md3v@gmail.com>
2023-05-31 12:42:34 +0530

    bruh moment 12

--- core/globals.py
+++ core/globals.py
@@ -1,5 +1,7 @@
-import torch
 import onnxruntime
 
 use_gpu = False
 providers = onnxruntime.get_available_providers()
+
+if 'TensorrtExecutionProvider' in providers:
+    providers.remove('TensorrtExecutionProvider')
