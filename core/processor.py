ab2fc1c221b78b2a57f2e971cf5064da089b576f
Somdev Sangwan <s0md3v@gmail.com>
2023-05-31 18:24:11 +0530

    Update processor.py

--- core/processor.py
+++ core/processor.py
@@ -2,6 +2,7 @@ import os
 
 import cv2
 import insightface
+import core.globals
 from core.config import get_face
 
 FACE_SWAPPER = None
@@ -11,7 +12,7 @@ def get_face_swapper():
     global FACE_SWAPPER
     if FACE_SWAPPER is None:
         model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../inswapper_128.onnx')
-        FACE_SWAPPER = insightface.model_zoo.get_model(model_path)
+        FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=core.globals.providers)
     return FACE_SWAPPER
 
 
