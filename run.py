50283c88e0f1f39648c0a6f958d2fd6a651b62ba
henryruhs <info@henryruhs.com>
2023-05-31 19:27:31 +0200

    Fix key error

--- run.py
+++ run.py
@@ -192,7 +192,7 @@ def start():
         print("\n[WARNING] No face detected in source image. Please try with another one.\n")
         return
     if is_img(target_path):
-        if predict_image(args[target_path]) > 0.7:
+        if predict_image(target_path) > 0.7:
             quit()
         process_img(args['source_img'], target_path, args['output_file'])
         status("swap successful!")
