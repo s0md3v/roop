aed9610e9c227b28979a1bf9d5372f6c31736f2a
Bjorn Lammers <walkxnl@gmail.com>
2023-06-02 18:47:48 +0200

    🔀 Merge next changes into next-dev

--- roop/core.py
+++ roop/core.py
@@ -14,12 +14,10 @@ import tkinter as tk
 from tkinter import filedialog
 from opennsfw2 import predict_video_frames, predict_image
 from tkinter.filedialog import asksaveasfilename
-import webbrowser
 import psutil
 import cv2
 import threading
 from PIL import Image, ImageTk
-
 import roop.globals
 from roop.swapper import process_video, process_img
 from roop.utils import is_img, detect_fps, set_fps, create_video, add_audio, extract_frames, rreplace
@@ -46,9 +44,6 @@ parser.add_argument('--all-faces', help='swap all faces in frame', dest='all_fac
 for name, value in vars(parser.parse_args()).items():
     args[name] = value
 
-if '--all-faces' in sys.argv or '-a' in sys.argv:
-    roop.globals.all_faces = True
-
 sep = "/"
 if os.name == "nt":
     sep = "\\"
@@ -71,7 +66,7 @@ def pre_check():
         quit('Python version is not supported - please upgrade to 3.9 or higher')
     if not shutil.which('ffmpeg'):
         quit('ffmpeg is not installed!')
-    model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../inswapper_128.onnx')
+    model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'inswapper_128.onnx')
     if not os.path.isfile(model_path):
         quit('File "inswapper_128.onnx" does not exist!')
     if '--gpu' in sys.argv:
@@ -91,18 +86,24 @@ def pre_check():
                 quit(f"CUDNN version {CUDNN_VERSION} is not supported - please downgrade to 8.9.1")
     else:
         roop.globals.providers = ['CPUExecutionProvider']
-    if '--all-faces' in sys.argv or '-a' in sys.argv:
-        roop.globals.all_faces = True
 
 
 def start_processing():
     frame_paths = args["frame_paths"]
     n = len(frame_paths) // (args['cores_count'])
-    # single thread
+    # Single thread
     if args['gpu'] or n < 2:
         process_video(args['source_img'], args["frame_paths"])
         return
-    # multi thread if video frames to cpu cores ratio is 2
+    processes = []
+    for i in range(0, len(frame_paths), n):
+        p = pool.apply_async(process_video, args=(args['source_img'], frame_paths[i:i+n],))
+        processes.append(p)
+    for p in processes:
+        p.get()
+    pool.close()
+    pool.join()
+    # Multi thread if video frames to cpu cores ratio is 2
     if n > 2:
         processes = []
         for i in range(0, len(frame_paths), n):
@@ -119,7 +120,7 @@ def preview_image(image_path):
     img = img.resize((180, 180), Image.ANTIALIAS)
     photo_img = ImageTk.PhotoImage(img)
     left_frame = tk.Frame(window)
-    left_frame.place(x=60, y=100)
+    left_frame.place(x=50, y=50)
     img_label = tk.Label(left_frame, image=photo_img)
     img_label.image = photo_img
     img_label.pack()
@@ -137,7 +138,7 @@ def preview_video(video_path):
         img = img.resize((180, 180), Image.ANTIALIAS)
         photo_img = ImageTk.PhotoImage(img)
         right_frame = tk.Frame(window)
-        right_frame.place(x=360, y=100)
+        right_frame.place(x=370, y=50)
         img_label = tk.Label(right_frame, image=photo_img)
         img_label.image = photo_img
         img_label.pack()
@@ -210,7 +211,7 @@ def start():
         quit()
     video_name_full = target_path.split("/")[-1]
     video_name = os.path.splitext(video_name_full)[0]
-    output_dir = os.path.dirname(target_path) + "/" + video_name
+    output_dir = os.path.dirname(target_path) + "/" + video_name if os.path.dirname(target_path) else video_name
     Path(output_dir).mkdir(exist_ok=True)
     status("detecting video's FPS...")
     fps, exact_fps = detect_fps(target_path)
@@ -237,7 +238,7 @@ def start():
     status("swap successful!")
 
 
-def run():
+if __name__ == "__main__":
     global status_label, window
 
     pre_check()
@@ -247,46 +248,42 @@ def run():
         args['cli_mode'] = True
         start()
         quit()
+
     window = tk.Tk()
     window.geometry("600x700")
-    window.title("roop")
-    window.configure(bg="#2d3436")
+    window.title("Roop - Face Replacement Software")
+    window.configure(bg="#161617")
     window.resizable(width=False, height=False)
 
-    # Contact information
-    support_link = tk.Label(window, text="Donate to project <3", fg="#fd79a8", bg="#2d3436", cursor="hand2", font=("Arial", 8))
-    support_link.place(x=180,y=20,width=250,height=30)
-    support_link.bind("<Button-1>", lambda e: webbrowser.open("https://github.com/sponsors/s0md3v"))
-
     # Select a face button
-    face_button = tk.Button(window, text="Select a face", command=select_face, bg="#2d3436", fg="#74b9ff", highlightthickness=4, relief="flat", highlightbackground="#74b9ff", activebackground="#74b9ff", borderwidth=4)
-    face_button.place(x=60,y=320,width=180,height=80)
+    face_button = tk.Button(window, text="Select a face", command=select_face, bg="#1f1f21", fg="#8442ff", relief="flat", activeforeground="#8442ff", padx=10, pady=10, highlightbackground="#8442ff", highlightcolor="#8442ff", highlightthickness=2, font=("Arial", 10))
+    face_button.place(relx=0.17, rely=0.42, anchor=tk.CENTER)
 
     # Select a target button
-    target_button = tk.Button(window, text="Select a target", command=select_target, bg="#2d3436", fg="#74b9ff", highlightthickness=4, relief="flat", highlightbackground="#74b9ff", activebackground="#74b9ff", borderwidth=4)
-    target_button.place(x=360,y=320,width=180,height=80)
+    target_button = tk.Button(window, text="Select a target", command=select_target, bg="#1f1f21", fg="#8442ff", relief="flat", activeforeground="#8442ff", padx=10, pady=10, highlightbackground="#8442ff", highlightcolor="#8442ff", highlightthickness=2, font=("Arial", 10))
+    target_button.place(relx=0.83, rely=0.42, anchor=tk.CENTER)
 
     # All faces checkbox
     all_faces = tk.IntVar()
-    all_faces_checkbox = tk.Checkbutton(window, anchor="w", relief="groove", activebackground="#2d3436", activeforeground="#74b9ff", selectcolor="black", text="Process all faces in frame", fg="#dfe6e9", borderwidth=0, highlightthickness=0, bg="#2d3436", variable=all_faces, command=toggle_all_faces)
-    all_faces_checkbox.place(x=60,y=500,width=240,height=31)
+    all_faces_checkbox = tk.Checkbutton(window, anchor="w", relief="groove", activebackground="#161617", activeforeground="#8442ff", selectcolor="black", text="Process all faces in frame", fg="#8442ff", bd=0, highlightthickness=0, bg="#161617", variable=all_faces, command=toggle_all_faces)
+    all_faces_checkbox.place(x=60, y=500, width=240, height=31)
 
     # FPS limit checkbox
     limit_fps = tk.IntVar(None, not args['keep_fps'])
-    fps_checkbox = tk.Checkbutton(window, anchor="w", relief="groove", activebackground="#2d3436", activeforeground="#74b9ff", selectcolor="black", text="Limit FPS to 30", fg="#dfe6e9", borderwidth=0, highlightthickness=0, bg="#2d3436", variable=limit_fps, command=toggle_fps_limit)
-    fps_checkbox.place(x=60,y=475,width=240,height=31)
+    fps_checkbox = tk.Checkbutton(window, anchor="w", relief="groove", activebackground="#161617", activeforeground="#8442ff", selectcolor="black", text="Limit FPS to 30", fg="#8442ff", bd=0, highlightthickness=0, bg="#161617", variable=limit_fps, command=toggle_fps_limit)
+    fps_checkbox.place(x=60, y=475, width=240, height=31)
 
     # Keep frames checkbox
     keep_frames = tk.IntVar(None, args['keep_frames'])
-    frames_checkbox = tk.Checkbutton(window, anchor="w", relief="groove", activebackground="#2d3436", activeforeground="#74b9ff", selectcolor="black", text="Keep frames dir", fg="#dfe6e9", borderwidth=0, highlightthickness=0, bg="#2d3436", variable=keep_frames, command=toggle_keep_frames)
-    frames_checkbox.place(x=60,y=450,width=240,height=31)
+    frames_checkbox = tk.Checkbutton(window, anchor="w", relief="groove", activebackground="#161617", activeforeground="#8442ff", selectcolor="black", text="Keep frames dir", fg="#8442ff", bd=0, highlightthickness=0, bg="#161617", variable=keep_frames, command=toggle_keep_frames)
+    frames_checkbox.place(x=60, y=450, width=240, height=31)
 
     # Start button
-    start_button = tk.Button(window, text="Start", bg="#f1c40f", relief="flat", borderwidth=0, highlightthickness=0, command=lambda: [save_file(), start()])
-    start_button.place(x=240,y=560,width=120,height=49)
+    start_button = tk.Button(window, text="Start", bg="#814ce6", fg="#161617", relief="flat", padx=20, pady=10, command=lambda: [save_file(), start()], highlightbackground="#814ce6", highlightcolor="#814ce6", highlightthickness=2, font=("Arial", 12, "bold"))
+    start_button.place(relx=0.5, rely=0.88, anchor=tk.CENTER)
 
     # Status label
-    status_label = tk.Label(window, width=580, justify="center", text="Status: waiting for input...", fg="#2ecc71", bg="#2d3436")
-    status_label.place(x=10,y=640,width=580,height=30)
+    status_label = tk.Label(window, width=580, justify="center", text="Status: waiting for input...", fg="#2ecc71", bg="#161617")
+    status_label.place(x=10, y=640, width=580, height=30)
 
     window.mainloop()
