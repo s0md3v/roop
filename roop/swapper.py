12f093b5ff909132e05276b21e238dc759fd78ac
Pikachu~~~ <Moeblack@users.noreply.github.com>
2023-06-04 18:12:34 +0800

    Add files via upload

--- roop/swapper.py
+++ roop/swapper.py
@@ -1,31 +1,27 @@
+
 import os
 from tqdm import tqdm
-import torch
 import onnxruntime
 import cv2
 import insightface
 import threading
 import roop.globals
 from roop.analyser import get_face_single, get_face_many
-from roop.globals import gpu_vendor
 
 FACE_SWAPPER = None
-lock = threading.Lock()
+THREAD_LOCK = threading.Lock()
+
 
 def get_face_swapper():
     global FACE_SWAPPER
-    with lock:
+    with THREAD_LOCK:
         if FACE_SWAPPER is None:
             model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../inswapper_128.onnx')
-            # If use nvidia-gpu, change implementation method.
-            if roop.globals.gpu_vendor == 'nvidia':
+            if roop.globals.gpu_vendor is not None:
                 FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=roop.globals.providers)
             else:
                 session_options = onnxruntime.SessionOptions()
-                if roop.globals.gpu_vendor is not None:
-                    session_options.intra_op_num_threads = roop.globals.gpu_threads
-                else:
-                    session_options.intra_op_num_threads = roop.globals.cpu_threads
+                session_options.intra_op_num_threads = roop.globals.cpu_threads
                 session_options.execution_mode = onnxruntime.ExecutionMode.ORT_PARALLEL
                 FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=roop.globals.providers, session_options=session_options)
     return FACE_SWAPPER
@@ -36,15 +32,6 @@ def swap_face_in_frame(source_face, target_face, frame):
         return get_face_swapper().get(frame, target_face, source_face, paste_back=True)
     return frame
 
-def process_frames(source_face, frame_paths, progress):
-    for frame_path in frame_paths:
-        frame = cv2.imread(frame_path)
-        try:
-            result = process_faces(source_face, frame)
-            cv2.imwrite(frame_path, result)
-        except Exception:
-            pass
-        progress.update(1)
 
 def process_faces(source_face, target_frame):
     if roop.globals.all_faces:
@@ -59,38 +46,43 @@ def process_faces(source_face, target_frame):
     return target_frame
 
 
-def process_video(source_img, frame_paths, preview_callback):
-    source_face = get_face_single(cv2.imread(source_img))
-    progress_bar_format = '{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]'
+def process_frames(source_face, frame_paths, progress):
+    for frame_path in frame_paths:
+        frame = cv2.imread(frame_path)
+        try:
+            result = process_faces(source_face, frame)
+            cv2.imwrite(frame_path, result)
+        except Exception:
+            pass
+        progress.update(1)
 
-    with tqdm(total=len(frame_paths), desc="Processing", unit="frame", dynamic_ncols=True, bar_format=progress_bar_format) as progress:
-        # nvidia multi-threading
-        if roop.globals.gpu_vendor == 'nvidia':
-            progress.set_postfix(cuda_utilization="{:02d}%".format(torch.cuda.utilization()), cuda_memory="{:02d}GB".format(torch.cuda.memory_usage()))
-            # caculate the number of frames each threads processed
-            num_threads = roop.globals.gpu_threads
-            num_frames_per_thread = len(frame_paths) // num_threads
-            remaining_frames = len(frame_paths) % num_threads
-            # create thread list
-            threads = []
-            start_index = 0
-            # create thread and launch
-            for _ in range(num_threads):
-                end_index = start_index + num_frames_per_thread
-                if remaining_frames > 0:
-                    end_index += 1
-                    remaining_frames -= 1
-                thread_frame_paths = frame_paths[start_index:end_index]
-                thread = threading.Thread(target=process_frames, args=(source_face, thread_frame_paths, progress))
-                threads.append(thread)
-                thread.start()
-                start_index = end_index
-            # threading
-            for thread in threads:
-                thread.join()
-        else:
-            # thread by Henry
-            process_frames(source_img, frame_paths, progress)
+
+def multi_process_frame(source_face,frame_paths,progress):
+
+    # caculate the number of frames each threads processed
+    num_threads = roop.globals.gpu_threads
+    num_frames_per_thread = len(frame_paths) // num_threads
+    remaining_frames = len(frame_paths) % num_threads
+    
+    # initialize thread list
+    threads = []
+            
+    # create thread and launch
+    start_index = 0
+    for _ in range(num_threads):
+        end_index = start_index + num_frames_per_thread
+        if remaining_frames > 0:
+            end_index += 1
+            remaining_frames -= 1
+        thread_frame_paths = frame_paths[start_index:end_index]
+        thread = threading.Thread(target=process_frames, args=(source_face, thread_frame_paths, progress))
+        threads.append(thread)
+        thread.start()
+        start_index = end_index
+
+    # threading
+    for thread in threads:
+        thread.join()
 
 
 def process_img(source_img, target_path, output_file):
@@ -100,3 +92,14 @@ def process_img(source_img, target_path, output_file):
     result = get_face_swapper().get(frame, face, source_face, paste_back=True)
     cv2.imwrite(output_file, result)
     print("\n\nImage saved as:", output_file, "\n\n")
+
+
+def process_video(source_img, frame_paths):
+    source_face = get_face_single(cv2.imread(source_img))
+    progress_bar_format = '{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]'
+
+    with tqdm(total=len(frame_paths), desc="Processing", unit="frame", dynamic_ncols=True, bar_format=progress_bar_format) as progress:
+        if roop.globals.gpu_vendor is not None:
+            multi_process_frame(source_face,frame_paths,progress)
+        else:
+            process_frames(source_img, frame_paths, progress)
