c59400f6a6aa5a21836f8245b5e148cd9421da2b
Phan Tuấn Anh <phantuananh@hotmail.com.vn>
2023-06-05 11:58:24 +0200

    clear CUDA cache after swapping
    on low VRAM + ffmpeg cuda acceleration, clearing cache prevent cuda out-of-memory error

--- roop/core.py
+++ roop/core.py
@@ -209,6 +209,7 @@ def start(preview_callback = None):
         process_video_multi_cores(args.source_img, args.frame_paths)
     else:
         process_video(args.source_img, args.frame_paths)
+    torch.cuda.empty_cache() 
     status("creating video...")
     create_video(video_name, exact_fps, output_dir)
     status("adding audio...")
