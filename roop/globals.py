419458485431a899859bbff2b06fa63037495780
henryruhs <info@henryruhs.com>
2023-06-06 19:29:00 +0200

    Move every UI related thing to ui.py

--- roop/globals.py
+++ roop/globals.py
@@ -1,5 +1,8 @@
 import onnxruntime
 
+source_path = None
+target_path = None
+output_path = None
 keep_fps = None
 keep_audio = None
 keep_frames = None
@@ -7,6 +10,7 @@ all_faces = None
 cpu_cores = None
 gpu_threads = None
 gpu_vendor = None
+max_memory = None
 headless = None
 log_level = 'error'
 providers = onnxruntime.get_available_providers()
