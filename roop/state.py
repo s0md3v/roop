34c335d6bd228149f7487f5cf37651d34f8c2ad7
Pozitronik <antikillerxxl@gmail.com>
2023-06-08 14:16:32 +0400

    issue #377 save global variables into the state

--- roop/state.py
+++ roop/state.py
@@ -1,10 +1,37 @@
 import roop.globals
 import pickle
+import os
+
+state_struct = {
+    'parameters': {},
+    'step': None,
+    'frame_sets': {}
+}
+
+
+def prepare_state():
+    all_variables = dir(roop.globals)
+    filtered_variables = {
+        var: getattr(roop.globals, var) for var in all_variables
+        if var not in ['onnxruntime', 'providers'] and not var.startswith('__')
+    }
+
+    roop.state.state_struct = {
+        'parameters': filtered_variables,
+        'step': None,
+        'frame_sets': {}
+    }
+
 
 def save_state() -> None:
+    prepare_state()
     with open('.state', 'wb') as file:
-        pickle.dump(roop.globals, file)
+        pickle.dump(state_struct, file)
+
 
 def load_state(state_path: str = None) -> bool:
-   if state_path is None: state_path = '.state'
-   roop.globals = pickle.load(state_path)
+    if state_path is None: state_path = '.state'
+    if not os.path.exists(): return False
+    with open(state_path, 'rb') as file:
+        roop.state.state_struct = pickle.load(file)
+    return True
