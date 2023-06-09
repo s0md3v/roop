3c6e560f18f0b03fce099ca7b5be7490ab54b4ab
Pozitronik <antikillerxxl@gmail.com>
2023-06-09 18:25:46 +0400

    issue #377 changed state file format to human-readable json added exists() routine fixes

--- roop/state.py
+++ roop/state.py
@@ -1,5 +1,5 @@
 import roop.globals
-import pickle
+import json
 import os
 import re
 from typing import List
@@ -9,10 +9,8 @@ swapping_in_progress: bool = False
 
 state_struct = {
     'globals': {},
-    'frames': {}
+    'frames': []
 }
-
-processed_frames = []
 
 
 def prepare_state() -> None:
@@ -20,14 +18,9 @@ def prepare_state() -> None:
     prepares state data (collects globals variables and processed frames numbers)
     """
     all_variables = dir(roop.globals)
-    filtered_variables = {
+    roop.state.state_struct['globals'] = {
         var: getattr(roop.globals, var) for var in all_variables
         if var not in ['onnxruntime', 'providers'] and not var.startswith('__')
-    }
-
-    roop.state.state_struct = {
-        'globals': filtered_variables,
-        'frames': processed_frames
     }
 
 
@@ -40,8 +33,8 @@ def save_state(state_path: str = '.state') -> bool:
         if os.path.exists(state_path): os.remove(state_path)
         return False
     prepare_state()
-    with open(state_path, 'wb') as file:
-        pickle.dump(state_struct, file)
+    with open(state_path, 'w') as file:
+        json.dump(state_struct, file)
     return True
 
 
@@ -52,8 +45,8 @@ def load_state(state_path: str = '.state') -> bool:
     :return: if the state file is exists and loaded successfully
     """
     if not os.path.exists(state_path): return False
-    with open(state_path, 'rb') as file:
-        roop.state.state_struct = pickle.load(file)
+    with open(state_path, 'r') as file:
+        roop.state.state_struct = json.load(file)
         for variable_name, variable_value in roop.state.state_struct['globals'].items():
             if hasattr(roop.globals, variable_name):
                 setattr(roop.globals, variable_name, variable_value)
@@ -65,7 +58,7 @@ def mark_frame_processed(frame_name: str) -> None:
     marks passed frame as processed
     :param frame_name:
     """
-    roop.state.processed_frames += [re.findall(r'\d+', frame_name)[0]]
+    roop.state.state_struct['frames'].append(re.findall(r'\d+', frame_name)[0])
     save_state()
 
 
@@ -77,3 +70,12 @@ def prepare_frames(frames_paths: List) -> List:
     """
     frames_paths = [x for x in frames_paths if not re.findall(r'\d+', x)[0] in roop.state.state_struct['frames']]
     return frames_paths
+
+
+def exists(target_path: str) -> bool:
+    """
+    checks if the current state is for target_path, and frames are already extracted
+    :param target_path: target path
+    :return: if state is exists
+    """
+    return 'target_path' in roop.state.state_struct['globals'] and roop.state.state_struct['globals']['target_path'] == target_path and roop.state.state_struct['frames']
