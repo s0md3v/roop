772d3ab424a7e678e9d957882cb1852bfb58759d
henryruhs <info@henryruhs.com>
2023-06-09 22:32:18 +0200

    Introduce execution provider to support DirectML

--- roop/utilities.py
+++ roop/utilities.py
@@ -38,8 +38,9 @@ def extract_frames(target_path: str) -> None:
 
 
 def create_video(target_path: str, fps: int) -> None:
+    temp_output_path = get_temp_output_path(target_path)
     temp_directory_path = get_temp_directory_path(target_path)
-    run_ffmpeg(['-i', os.path.join(temp_directory_path, '%04d.png'), '-framerate', str(fps), '-c:v', roop.globals.video_encoder, '-crf', str(roop.globals.video_quality), '-pix_fmt', 'yuv420p', '-y', get_temp_output_path(target_path)])
+    run_ffmpeg(['-i', os.path.join(temp_directory_path, '%04d.png'), '-framerate', str(fps), '-c:v', roop.globals.video_encoder, '-crf', str(roop.globals.video_quality), '-pix_fmt', 'yuv420p', '-y', temp_output_path])
 
 
 def restore_audio(target_path: str, output_path: str) -> None:
