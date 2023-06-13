95f4582a991d182c694d531db68ef366fd65d4f8
Moeblack <Moeblack@kuroinekorachi@gmail.com>
2023-06-13 22:36:43 +0800

    modified core.py ui.py frame/core.py

--- roop/processors/frame/core.py
+++ roop/processors/frame/core.py
@@ -2,15 +2,21 @@ import sys
 import importlib
 from typing import Any
 
-FRAME_PROCESSOR_MODULE = None
+FRAME_PROCESSORS_MODULES = None
+
+def load_processor_module(frame_processor: str)-> Any:
+    try:
+        frame_processor_module = importlib.import_module(f'roop.processors.frame.{frame_processor}')
+    except ImportError:
+        sys.exit()
+    return frame_processor_module
 
 
-def get_frame_processor_module(frame_processor: str) -> Any:
-    global FRAME_PROCESSOR_MODULE
-
-    if not FRAME_PROCESSOR_MODULE:
-        try:
-            FRAME_PROCESSOR_MODULE = importlib.import_module(f'roop.processors.frame.{frame_processor}')
-        except ImportError:
-            sys.exit()
-    return FRAME_PROCESSOR_MODULE
+def get_frame_processor_modules(frame_processors):
+    global FRAME_PROCESSORS_MODULES
+    if FRAME_PROCESSORS_MODULES is None:
+        FRAME_PROCESSORS_MODULES = []
+        for frame_processor in frame_processors:
+            frame_processor_module = load_processor_module(frame_processor)
+            FRAME_PROCESSORS_MODULES.append(frame_processor_module)
+        return FRAME_PROCESSORS_MODULES
