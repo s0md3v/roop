3c4a1f7f777547c15d9fcbaacde30be3aa9d5a9f
Moeblack <Moeblack@kuroinekorachi@gmail.com>
2023-06-15 10:32:05 +0800

    Code Refactoring for Enhancer

--- roop/processors/frame/face_enhancer.py
+++ roop/processors/frame/face_enhancer.py
@@ -2,13 +2,15 @@ from typing import List, Any
 import cv2
 import torch
 import threading
+import numpy as np
 from torchvision.transforms.functional import normalize
-from codeformer.facelib.utils.face_restoration_helper import FaceRestoreHelper
 from codeformer.basicsr.utils.registry import ARCH_REGISTRY
 from codeformer.basicsr.utils import img2tensor, tensor2img
+from insightface.utils.face_align import norm_crop2
 
 import roop.globals
 import roop.processors.frame.core
+from roop.face_analyser import get_many_faces
 from roop.utilities import conditional_download, resolve_relative_path
 
 if 'ROCMExecutionProvider' in roop.globals.execution_providers:
@@ -43,58 +45,30 @@ def get_code_former():
             CODE_FORMER.load_state_dict(model)
             CODE_FORMER.eval()
         return CODE_FORMER
+
+
+def align_warp_face(temp_frame):
+    many_faces = get_many_faces(temp_frame)
+    if many_faces:
+        face_list = []
+        matrix_list = []
+        for target_face in many_faces:
+            face_long_side = max(target_face.bbox[2] - target_face.bbox[0], target_face.bbox[3] - target_face.bbox[1])
+            face_size = int(np.ceil(face_long_side / 128) * 128)
+            face, Matrix = norm_crop2(temp_frame, target_face.kps, face_size)
+            face = cv2.resize(face, (512, 512), interpolation=cv2.INTER_LINEAR)
+            upscale_factor = 512 / face_size
+            Matrix *= upscale_factor
+            face_list.append(face)
+            matrix_list.append(Matrix)
+        return face_list, matrix_list
     
-
-def get_face_enhancer():
-    global FACE_ENHANCER
-
-    with THREAD_LOCK:
-        if FACE_ENHANCER is None:
-            FACE_ENHANCER = FaceRestoreHelper(
-                upscale_factor=int(2),
-                face_size=512,
-                crop_ratio=(1, 1),
-                det_model='retinaface_resnet50',
-                save_ext='png',
-                use_parse=True,
-                device='cuda'
-            )
-    return FACE_ENHANCER
-
-
-def enhance_face_in_frame(cropped_faces):
-    faces_enhanced = []
-    for _, cropped_face in enumerate(cropped_faces):
-        face_in_tensor = normalize_face(cropped_face)
-        face_enhanced = restore_face(face_in_tensor)
-        faces_enhanced.append(face_enhanced)
-    return faces_enhanced
-
-
-def process_frame(source_face: any, temp_frame: any) -> any:
-    THREAD_SEMAPHORE.acquire()
-    face_helper = get_face_enhancer()
-    face_helper.read_image(temp_frame)
-    # get face landmarks for each face
-    face_helper.get_face_landmarks_5(only_center_face=False, resize=640, eye_dist_threshold=5)
-    # align and warp each face
-    face_helper.align_warp_face()
-    cropped_faces = face_helper.cropped_faces
-    faces_enhanced = enhance_face_in_frame(cropped_faces)
-    for face_enhanced in faces_enhanced:
-        face_helper.add_restored_face(face_enhanced)
-    face_helper.get_inverse_affine()
-    result = face_helper.paste_faces_to_input_image()
-    face_helper.clean_all()
-    THREAD_SEMAPHORE.release()
-    return result
-
 
 def normalize_face(face):
     face_in_tensor = img2tensor(face / 255.0, bgr2rgb=True, float32=True)
     normalize(face_in_tensor, (0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True)
     return face_in_tensor.unsqueeze(0).to('cuda')
-
+      
 
 def enhance_face_in_tensor(face_in_tensor, codeformer_fidelity = 0.6):
     with torch.no_grad():
@@ -114,6 +88,53 @@ def restore_face(face_in_tensor) -> Any:
     except RuntimeError:
         pass
     return convert_tensor_to_image(face_in_tensor)
+
+
+def enhance_face_in_frame(face_list):
+    face_enhanced_list = []
+    for  _, cropped_face in enumerate(face_list):
+        face_in_tensor = normalize_face(cropped_face)
+        face_enhanced = restore_face(face_in_tensor)
+        face_enhanced_list.append(face_enhanced)
+    return face_enhanced_list
+
+
+def paste_face_back(temp_frame, face_enhanced_list, matrix_list):
+    target_img = temp_frame
+    for i, face_enhanced in enumerate(face_enhanced_list):
+        matrix = matrix_list[i]
+        face_enhanced_height = face_enhanced.shape[0]
+        face_enhanced_width = face_enhanced.shape[1]
+        inverse_matrix = cv2.invertAffineTransform(matrix)
+        face_enhanced = cv2.warpAffine(face_enhanced, inverse_matrix, (target_img.shape[1], target_img.shape[0]), borderValue=0.0)
+        image_white = np.full((face_enhanced_height,face_enhanced_width), 255, dtype=np.float32)
+        image_white = cv2.warpAffine(image_white, inverse_matrix, (target_img.shape[1], target_img.shape[0]), borderValue=0.0)
+        image_white[image_white>20] = 255
+        image_mask = image_white
+        mask_row_indices, mask_col_indices = np.where(image_mask==255)
+        mask_height = np.max(mask_row_indices) - np.min(mask_row_indices)
+        mask_width = np.max(mask_col_indices) - np.min(mask_col_indices)
+        mask_size = int(np.sqrt(mask_height*mask_width))
+        k_threshold = max(mask_size//10, 10)
+        kernel = np.ones((k_threshold,k_threshold),np.uint8)
+        image_mask = cv2.erode(image_mask,kernel,iterations = 1)
+        k_threshold = max(mask_size//20, 5)
+        kernel_size = (k_threshold, k_threshold)
+        blur_size = tuple(2*i+1 for i in kernel_size)
+        image_mask = cv2.GaussianBlur(image_mask, blur_size, 0)
+        image_mask /= 255
+        image_mask = np.reshape(image_mask, [image_mask.shape[0],image_mask.shape[1],1])
+        image_merged = image_mask * face_enhanced + (1-image_mask) * target_img.astype(np.float32)
+        image_merged = image_merged.astype(np.uint8)
+        target_img = image_merged
+    return target_img
+
+
+def process_frame(source_face: any, temp_frame: any) -> any:
+    face_list, matrix_list = align_warp_face(temp_frame)
+    face_enhanced_list = enhance_face_in_frame(face_list)
+    result = paste_face_back(temp_frame, face_enhanced_list, matrix_list)
+    return result
 
 
 def process_frames(source_path: str, frame_paths: list[str], progress=None) -> None:
