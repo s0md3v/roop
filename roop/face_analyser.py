fb31af0596b40c55348187f787005195c1192383
henryruhs <info@henryruhs.com>
2023-07-09 20:16:03 +0200

    Prevent AttributeError if face is None

--- roop/face_analyser.py
+++ roop/face_analyser.py
@@ -46,7 +46,8 @@ def get_many_faces(frame: Frame) -> Optional[List[Face]]:
 def find_similar_face(frame: Frame, reference_face: Face) -> Optional[Face]:
     faces = get_many_faces(frame)
     for face in faces:
-        distance = numpy.sum(numpy.square(face.normed_embedding - reference_face.normed_embedding))
-        if distance < roop.globals.similar_face_distance:
-            return face
+        if hasattr(face, 'normed_embedding') and hasattr(reference_face, 'normed_embedding'):
+            distance = numpy.sum(numpy.square(face.normed_embedding - reference_face.normed_embedding))
+            if distance < roop.globals.similar_face_distance:
+                return face
     return None
