9aa50236422a3705eb0304b83b1ed01faddac841
henryruhs <info@henryruhs.com>
2023-07-16 00:29:06 +0200

    ignore

--- roop/predictor.py
+++ roop/predictor.py
@@ -6,7 +6,19 @@ from keras import Model
 
 from roop.typing import Frame
 
+PREDICTOR = None
+THREAD_LOCK = threading.Lock()
 MAX_PROBABILITY = 0.85
+
+
+def get_predictor() -> Model:
+    global PREDICTOR
+
+    with THREAD_LOCK:
+        if PREDICTOR is None:
+            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    return PREDICTOR
+
 
 def clear_predictor() -> None:
     global PREDICTOR
