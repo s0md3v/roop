28002db86b3a4bc23693499acd5fcfac59a94a34
Lahfir <nmhlahfir2@gmail.com>
2023-07-20 11:14:48 +0530

    Added Quality Scaler and User Alert for Missing Input Files

--- roop/ui.py
+++ roop/ui.py
@@ -11,11 +11,7 @@ import roop.globals
 import roop.metadata
 from roop.face_analyser import get_one_face
 from roop.capturer import get_video_frame, get_video_frame_total
-from roop.face_reference import (
-    get_face_reference,
-    set_face_reference,
-    clear_face_reference,
-)
+from roop.face_reference import get_face_reference, set_face_reference, clear_face_reference
 from roop.predictor import predict_frame, clear_predictor
 from roop.processors.frame.core import get_frame_processors_modules
 from roop.utilities import is_image, is_video, resolve_relative_path
