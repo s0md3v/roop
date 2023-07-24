932749a42f5f65e1d46f840d64340b1906912810
henryruhs <info@henryruhs.com>
2023-07-24 13:03:10 +0200

    Add theme

--- roop/uis/core.py
+++ roop/uis/core.py
@@ -3,10 +3,18 @@ from roop.uis import source, target, preview, output
 
 
 def init() -> None:
-    with gradio.Blocks() as ui:
+    with gradio.Blocks(theme=get_theme()) as ui:
         with gradio.Row():
             source.render()
             target.render()
         preview.render()
         output.render()
     ui.launch()
+
+
+def get_theme() -> gradio.Theme:
+    return gradio.themes.Soft(
+        primary_hue=gradio.themes.colors.red,
+        secondary_hue=gradio.themes.colors.gray,
+        font=[gradio.themes.GoogleFont('Open Sans'), 'Arial', 'sans-serif']
+    )
