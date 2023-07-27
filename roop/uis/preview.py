653a82d6f801b8979afaf65798cda180fda51ab3
henryruhs <info@henryruhs.com>
2023-07-27 14:34:55 +0200

    Finish basic preview

--- roop/uis/preview.py
+++ roop/uis/preview.py
@@ -12,27 +12,34 @@ from roop.predictor import predict_frame
 from roop.processors.frame.core import get_frame_processors_modules
 from roop.typing import Frame
 from roop.uis import core as ui
-from roop.utilities import is_video
+from roop.utilities import is_video, is_image
 
 NAME = 'ROOP.UIS.PREVIEW'
 
 
 def render() -> None:
     with gradio.Column():
+        is_target_image = is_image(roop.globals.target_path)
         is_target_video = is_video(roop.globals.target_path)
-        preview_image = gradio.Image(
-            label='preview_image',
-            value=normalize_preview_frame(get_preview_frame(roop.globals.reference_frame_number)) if is_target_video else None
-        )
+        if is_target_image:
+            temp_frame = cv2.imread(roop.globals.target_path)
         if is_target_video:
             video_frame_total = get_video_frame_total(roop.globals.target_path)
-            preview_slider = gradio.Slider(
-                label='preview_slider',
-                value=roop.globals.reference_frame_number,
-                maximum=video_frame_total
-            )
-            preview_slider.change(update, inputs=preview_slider, outputs=[preview_image, preview_slider], show_progress=False)
-
+            temp_frame = get_video_frame(roop.globals.target_path, roop.globals.reference_frame_number)
+        preview_frame = get_preview_frame(temp_frame)
+        preview_image = gradio.Image(
+            label='preview_image',
+            value=normalize_preview_frame(preview_frame),
+            height=400,
+            visible=is_target_image or is_target_video
+        )
+        preview_slider = gradio.Slider(
+            label='preview_slider',
+            value=roop.globals.reference_frame_number if is_target_video else 0,
+            maximum=video_frame_total if is_target_video else 1,
+            visible=is_target_video
+        )
+        preview_slider.change(update, inputs=preview_slider, outputs=[preview_image, preview_slider], show_progress=False)
         source_file = ui.get_component('source_file')
         target_file = ui.get_component('target_file')
         if source_file:
@@ -43,24 +50,29 @@ def render() -> None:
 
 def update(frame_number: int = 0) -> Tuple[Dict[Any, Any], Dict[Any, Any]]:
     sleep(0.1)
-    if roop.globals.source_path and roop.globals.target_path:
-        preview_frame = get_preview_frame(frame_number)
+    if is_image(roop.globals.target_path):
+        temp_frame = cv2.imread(roop.globals.target_path)
+        preview_frame = get_preview_frame(temp_frame)
         if preview_frame.any():
-            return gradio.update(value=normalize_preview_frame(preview_frame), visible=True), gradio.update(value=frame_number, visible=True)
-    return gradio.update(value=None, visible=False), gradio.update(value=frame_number, visible=False)
+            return gradio.update(value=normalize_preview_frame(preview_frame), visible=True), gradio.update(value=0, maximum=1, visible=False)
+    if is_video(roop.globals.target_path):
+        video_frame_total = get_video_frame_total(roop.globals.target_path)
+        temp_frame = get_video_frame(roop.globals.target_path, frame_number)
+        preview_frame = get_preview_frame(temp_frame)
+        if preview_frame.any():
+            return gradio.update(value=normalize_preview_frame(preview_frame), visible=True), gradio.update(value=frame_number, maximum=video_frame_total, visible=True)
+    return gradio.update(value=None, visible=False), gradio.update(value=0, maximum=1, visible=False)
 
 
-def get_preview_frame(frame_number: int = 0) -> Frame:
-    temp_frame = get_video_frame(roop.globals.target_path, frame_number)
+def get_preview_frame(temp_frame: Frame) -> Frame:
     if predict_frame(temp_frame):
         destroy()
-    source_face = get_one_face(cv2.imread(roop.globals.source_path))
-    if not get_face_reference():
+    source_face = get_one_face(cv2.imread(roop.globals.source_path)) if roop.globals.source_path else None
+    if not roop.globals.many_faces and not get_face_reference():
         reference_frame = get_video_frame(roop.globals.target_path, roop.globals.reference_frame_number)
         reference_face = get_one_face(reference_frame, roop.globals.reference_face_position)
         set_face_reference(reference_face)
-    else:
-        reference_face = get_face_reference()
+    reference_face = get_face_reference() if not roop.globals.many_faces else None
     for frame_processor in get_frame_processors_modules(roop.globals.frame_processors):
         temp_frame = frame_processor.process_frame(
             source_face,
