5dd8628d935837b9d3774c98f0667bd15ddde100
Somdev Sangwan <s0md3v@gmail.com>
2023-08-22 01:42:28 +0530

    replace download links

--- roop/processors/frame/face_enhancer.py
+++ roop/processors/frame/face_enhancer.py
@@ -43,7 +43,7 @@ def clear_face_enhancer() -> None:
 
 def pre_check() -> bool:
     download_directory_path = resolve_relative_path('../models')
-    conditional_download(download_directory_path, ['https://huggingface.co/henryruhs/roop/resolve/main/GFPGANv1.4.pth'])
+    conditional_download(download_directory_path, ['https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth'])
     return True
 
 
